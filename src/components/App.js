9c946be06638d64533066297b64170efd48e7550
Keagile Nduli <keagile1403@icloud.com>
2025-07-17 16:09:07 +0200

    creating dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,31 @@ import React from "react";
 import { app } from "../firebase"; 
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
-import AuthProvider from "../contexts/AuthContext";
+import AuthProvider  from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard"
 function App() {
-
-
   return (
-    <AuthProvider>
-       <Container className="d-flex align-items-center 
-     justify-content-center"
-     style={{ minHeight: "100vh"}}>
-
-    <div className="w-100" style={{maxWidth: "400px"}} >
-         <Signup/>
-    </div>
-        
-    </Container>
+    <Router>
+      <AuthProvider>
+        <Container 
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Switch>
 
 
-    </AuthProvider>
-    
- 
-
-  )
- 
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              
+              {/* Add more routes here as needed */}
+            </Switch>
+          </div>
+        </Container>
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
