d0fd63eb271f84b71ce4eb50b71f080d1d451bc7
Keagile Nduli <keagile1403@icloud.com>
2025-07-24 15:25:39 +0200

    added a login

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import { Container } from "react-bootstrap";
 import AuthProvider  from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard"
+import Login from "./Login";
+
 function App() {
   return (
     <Router>
@@ -20,6 +22,7 @@ function App() {
 
               <Route exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login}/>
               
               {/* Add more routes here as needed */}
             </Switch>
